563e57f3d88c30542c22cc925698f260ea0e2699
aashiryar <raiar@uci.edu>
2024-04-17 14:19:23 -0700

    Submission commit of HW 2

---
+++ src/student_gtests.cpp
@@ -0,0 +1,40 @@
+#include <gtest/gtest.h>
+#include <sstream>
+#include "word_count.hpp"
+
+using namespace std;
+
+TEST(WordCount, ToLowercase) {
+	string test = "Hello";
+	string test1 = "hEllo";
+	string test2 = "hello";
+	to_lowercase(test);
+	to_lowercase(test1);
+	to_lowercase(test2);
+	EXPECT_STREQ("hello", test.c_str());
+	EXPECT_STREQ("hello", test1.c_str());
+	EXPECT_STREQ("hello", test2.c_str());
+}
+TEST(WordCount, LoadStopWords) {
+	stringstream test("Hello bye");
+	const auto stop_words = load_stopwords(test);
+	EXPECT_TRUE(stop_words.contains("hello"));
+	EXPECT_FALSE(stop_words.contains("byebye"));
+}
+TEST(WordCount, CountWords) {
+	stringstream test("aa bb aa cc");
+	const auto counts = count_words(test, {});
+	EXPECT_EQ(counts.at("aa"), 2);
+	EXPECT_EQ(counts.at("bb"), 1);
+	EXPECT_EQ(counts.at("cc"), 1);
+	EXPECT_FALSE(counts.contains("dd"));
+}
+TEST(WordCount, OutputWordCounts) {
+	map<string, int> word_counts;
+	word_counts["aa"] = 1;
+	word_counts["bb"] = 2;
+	word_counts["cc"] = 3;
+	stringstream output;
+	output_word_counts(word_counts, output);
+	EXPECT_STREQ(output.str().c_str(), "aa 1\nbb 2\ncc 3\n");
+}
