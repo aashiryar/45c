0019193d16fc046cbaa224526f363978e0a5f226
aashiryar <raiar@uci.edu>
2024-04-17 11:55:21 -0700

    Submission commit of HW 2

--- src/word_count.cpp
+++ src/word_count.cpp
@@ -1,1 +1,47 @@
+
 #include "word_count.hpp"
+#include <iostream>
+#include <iosfwd>
+#include <map>
+#include <set>
+#include <string>
+
+void to_lowercase(std:: string& str) {
+	std::string nstr = "";
+	for (auto x: str) {
+		nstr +=(char)tolower(x);
+	}
+	str = nstr;
+}
+std::set<std::string> load_stopwords(std::istream& stopwords) {
+	std::set<std::string> words;
+	std::string word;
+	while (stopwords >> word) {
+		to_lowercase(word);
+		words.insert(word);
+	}
+	return words;
+}
+std::map<std::string, int> count_words(std::istream& document, const std::set<std::string>& stopwords) {
+	std::map<std::string, int> word_counts;
+	std::string word;
+	while (document >> word){
+		to_lowercase(word);
+		if(stopwords.find(word) == stopwords.end()) {
+			if (word_counts.count(word) != 0) {
+				word_counts[word]++;
+			}
+			else {
+				word_counts.insert({word, 1});
+			}
+		}
+	}
+	return word_counts;
+}
+void output_word_counts(const std::map<std::string, int>& word_counts, std::ostream& output) {
+	for (const auto& elem: word_counts) {
+		output << elem.first << " " << elem.second << "\n";
+	}
+}
+
+
--- src/word_count.hpp
+++ src/word_count.hpp
@@ -5,45 +5,9 @@
 #include <map>
 #include <set>
 #include <string>
-#include <iostream>
 
-inline void to_lowercase(std::string& str) {
-	std::string nstr = "";
-	for (auto x: str) {
-		nstr += (char)tolower(x);
-	}
-	str = nstr;
-}
-inline std::set<std::string> load_stopwords(std::istream& stopwords) {
-	std::set<std::string> words;
-	std::string word;
-	while (stopwords >>  word) {
-		to_lowercase(word);
-		words.insert(word);
-	}
-	return words;
-}
-inline std::map<std::string, int> count_words(std::istream& document, const std::set<std::string>& stopwords) {
-	std::map<std::string, int> words_counts;
-	std::string word;
-	while (document >> word) {
-		to_lowercase(word);
-		if (stopwords.find(word) == stopwords.end()) {
-			if (words_counts.count(word) != 0){
-				words_counts[word] ++;
-			}
-			else {
-				words_counts.insert({word, 1});
-			}
-		}
-	}
-	return words_counts;
-
-}	
-inline void output_word_counts(const std::map<std::string, int>& word_counts, std::ostream& output) {
-	for (const auto& elem: word_counts) {
-		output << elem.first << " " << elem.second << "\n";
-	}
-}
-
+void to_lowercase(std::string& str);
+std::set<std::string> load_stopwords(std::istream& stopwords);
+std::map<std::string, int> count_words(std::istream& document, const std::set<std::string>& stopwords);
+void output_word_counts(const std::map<std::string, int>& word_counts, std::ostream& output);
 #endif
