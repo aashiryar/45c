396f13d11b0cad9328bae1955c9a4a40174976f9
aashiryar <raiar@uci.edu>
2024-05-15 12:23:42 -0700

    fix

--- src/shape.cpp
+++ src/shape.cpp
@@ -3,6 +3,6 @@
 using namespace std;
 Shape::Shape(Point center, std::string name) : center(center), name(name) {}
 void Shape::print(std::ostream& out) const {
-    out << name << "at (" << center.x << ", " << center.y << ") area = " << area;
+    out << name << "at (" << center.x << ", " << center.y << ") area = " << area();
 }
 
