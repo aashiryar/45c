0efe95f535454809d7b41e14c238ae9626511af8
aashiryar <raiar@uci.edu>
2024-06-03 14:19:58 -0700

    fix

---
+++ src/map_array.hpp
@@ -0,0 +1,87 @@
+#ifndef MAP_ARRAY_HPP
+#define MAP_ARRAY_HPP
+
+#include <algorithm>
+#include <iterator>
+#include <ranges>
+#include <utility>
+#include <vector>
+
+template <typename Key, typename Value>
+class MapArray {
+public:
+    class ArrayIterator {
+    public:
+        using iterator_category = std::random_access_iterator_tag;
+        using value_type = std::pair<Key, Value>;
+        using difference_type = std::ptrdiff_t;
+        using pointer = std::pair<Key, Value>*;
+        using reference = std::pair<Key, Value>&;
+        explicit ArrayIterator(std::pair<Key, Value>* ptr = nullptr);
+        ArrayIterator& operator++(){
+            ++ptr;
+            return *this;
+        }
+        ArrayIterator& operator--() {
+            --ptr;
+            return *this;
+        }
+        ArrayIterator& operator++(int) {
+            ArrayIterator temp= *this;
+            ++(*this);
+            return temp;
+        }
+        ArrayIterator& operator--(int) {
+            ArrayIterator temp= *this;
+            --(*this);
+            return temp;
+        }
+        ArrayIterator& operator+=(difference_type d) {
+            ptr= ptr+d;
+            return *this;
+        }
+        ArrayIterator& operator-=(difference_type d) {
+            ptr = ptr -d;
+            return *this;
+        }
+        friend ArrayIterator operator+(ArrayIterator it, difference_type d) {
+            it = it+d;
+            return it;
+        }
+        friend ArrayIterator operator+(difference_type d, ArrayIterator it) {
+            return ArrayIterator(d+itr.ptr);
+        }
+        friend ArrayIterator operator-(ArrayIterator it, difference_type d) {
+            it = it - d;
+            return it;
+        }
+        friend difference_type operator-(ArrayIterator lhs, ArrayIterator rhs) {
+            return lhs.ptr - rhs.ptr;
+        }
+        auto operator<=>(const ArrayIterator& other) const = default;
+        std::pair<Key, Value>& operator*() const {
+            return *ptr;
+        }
+        std::pair<Key, Value>* operator->() const {
+            return ptr;
+        }
+        std::pair<Key, Value>& operator[](difference_type d) const {
+            return (ptr+d);
+        }
+    private:
+        std::pair<Key, Value>* ptr;
+    };
+    using value_type = std::pair<Key, Value>;
+    using iterator = ArrayIterator;
+    ArrayIterator begin() {
+        return ArrayIterator(data.data());
+    }
+    ArrayIterator end() {
+        return ArrayIterator(data.data() + data.size());
+    }
+    Value& operator[](const Key& key);
+private:
+    std::vector<std::pair<Key, Value>> data;
+};
+
+#endif
---
+++ src/set_list.hpp
@@ -0,0 +1,85 @@
+#ifndef SET_LIST_HPP
+#define SET_LIST_HPP
+#include <algortithm>
+#include <functional>
+#include <iterator>
+#include <memory>
+#include <ranges>
+
+template <typename T>
+class SetList {
+    struct ListNode {
+        T data;
+        std::shared_ptr<ListNode> next;
+    };
+public:
+    class ListIterator {
+    public:
+        using iterator_category = std::forward_iterator_tag;
+        using value_type = T;
+        using difference_type = std::ptrdiff_t;
+        using pointer = T*;
+        using reference = T&;
+        explicit ListIterator(std::shared_ptr<ListNode> ptr = nullptr) : ptr(ptr) {};
+        ListIterator& operator++() {
+            if (ptr != nullptr) {
+                ptr = ptr->next;
+            }
+            return *this;
+        }
+        ListIterator operator++(int) {
+            ListIterator temp = *this;
+            ++(*this);
+            return temp;
+        }
+        T& operator*() const {
+            return ptr->data;
+        }
+        T* operator->() const{
+            return &(ptr->data);
+        }
+
+        bool operator==(const ListIterator& other) const = default;
+    private:
+        std::shared_ptr<ListNode> ptr;
+    }
+    using value_type = T;
+    using iterator = ListIterator;
+    
+    SetList() = default;
+
+    template <std::ranges::input_range Rng>
+    explicit SetList(Rng&& rng) {
+        std::ranges::for_each(std::forward<Rng>(rng), std::bind_front(&SetList::insert, this));
+    }
+
+    ListIterator begin() {
+        return ListIterator(head);
+    }
+    ListIterator end() {
+        return ListIterator(nullptr);
+    }
+    bool contains(const T& value) {
+        std::shared_ptr<ListNode> current = head;
+        while (current != nullptr) {
+            if (current->data == value) {
+                return true;
+            }
+            current = current->next;
+        }
+        return false;
+    }
+    ListIterator insert(T value) {
+        for (auto iter = begin(); iter != end(); ++iter) {
+            if (*iter == value) {
+                return iter;
+            }
+        auto node = std::make_shared<ListNode>(std::move(value));
+        node->next = head;
+        head = node;
+        return ListIterator(head);
+    }
+private:
+    std::shared_ptr<ListNode> head = nullptr;
+};
+#endif
